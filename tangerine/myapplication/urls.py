aed7af9e2371c0cc584a587799fdff479974a193
bs5ht <bs5ht@virginia.edu>
2016-09-18 15:23:50 -0400

    modified fxns

--- tangerine/myapplication/urls.py
+++ tangerine/myapplication/urls.py
@@ -3,17 +3,17 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^api/users/get/[0-9]+/$', views.retrieve_or_modify_user_info),
-    url(r'^api/users/modify/[0-9]+/$', views.retrieve_or_modify_user_info),
-    url(r'^api/users/delete/[0-9]+/$', views.delete_user),
+    url(r'^api/users/get/[0-9]+/?$', views.retrieve_or_modify_user_info),
+    url(r'^api/users/modify/[0-9]+/?$', views.retrieve_or_modify_user_info),
+    url(r'^api/users/delete/[0-9]+/?$', views.delete_user),
     url(r'^api/users/create$', views.create_user),
-    url(r'^api/items/get/[0-9]+/$', views.retrieve_or_modify_item_info),
-    url(r'^api/items/modify/[0-9]+/$', views.retrieve_or_modify_item_info),
-    url(r'^api/items/delete/[0-9]+/$', views.retrieve_or_modify_item_info),
-    url(r'^api/items/create/$', views.retrieve_or_modify_item_info),
+    url(r'^api/items/get/[0-9]+/?$', views.retrieve_or_modify_item_info),
+    url(r'^api/items/modify/[0-9]+/?$', views.retrieve_or_modify_item_info),
+    url(r'^api/items/delete/[0-9]+/?$', views.retrieve_or_modify_item_info),
+    url(r'^api/items/create/?$', views.retrieve_or_modify_item_info),
 
     #match all other urls
-    url ('', views.other),
+    url ('', views.invalidURL),
     #url(r'^api/items/(?P[0-9]+)/$', views.retrieve_or_modify_item_info),
     
 ]
--- tangerine/myapplication/views.py
+++ tangerine/myapplication/views.py
@@ -1,36 +1,64 @@
 from django.core import serializers
 from .models import User, BicycleItem
 from django.http import HttpResponse
+import json
 # Create your views here.
 
 def index(request):
 
-    return HttpResponse("Hello, World.\n")
+    return HttpResponse("Welcome to tangerine.\n")
 #r'^api/users/(?P<uuid>[^/]+)/$'
 def retrieve_or_modify_user_info(request):
     if request.method == 'GET':
         url = request.path
-        usrStr = url.split("/")[-1] # make sure to check this code 
-        user = User.objects.get(userid = usrStr)
-        userJSON = serializers.serialize('json', [ user, ])
-        userJSON.status = True;
-        userJSON.message = "successfully retrieved user";
-
+        usrStr = url.split("/")[5] # make sure to check this code 
+        try:
+            user = User.objects.get(pk = usrStr)
+        except User.DoesNotExist:
+            user = None
+        if user == None:
+            userJSON = {}
+            userJSON['status'] = False
+            userJSON['message'] = "There is no user specified"
+            userJSON = json.dumps(userJSON)
+        else:
+            userJSON = serializers.serialize('json', [user,])
+            struct = json.loads(userJSON)
+            struct[0]['status'] = True
+            struct[0]['message'] = "Correctly obtained user"
+            userJSON = json.dumps(struct[0])           
         return HttpResponse(userJSON)
     elif request.method == 'POST':
         url = request.path
-        usrStr = url.split("/")[4]
-        user = User.objects.get(userid = usrStr)
-        for key in request:
-            user.key = request.POST.get(key, False)
-        user.save()
+        usrStr = url.split("/")[5]
+        user = None
+        try:
+            user = User.objects.get(pk = usrStr)
+        except User.DoesNotExist:
+            user = None
+        retJSON = {}
+        if user == None:
+            retJSON['status'] = False
+            retJSON['message'] = "There is no user specified"
+            retJSON = json.dumps(userJSON)
+        else:
+            for key in request:
+                user.key = request.POST.get(key, False)
+            user.save()
+            retJSON['status'] = True
+            retJSON['message'] = "Successfully modified user"
+            retJSON = json.dumps(retJSON)
+        return HttpResponse(retJSON)
 def create_user(request):
-    user = User.objects.create(userid=request.POST.get("userid", False))
+    user = User.objects.create()
     stringster = ""
     for key in request.POST:
         stringster +=key
         setattr(user, key, request.POST.get(key, False))
     user.save()
+    retJSON = {}
+    retJSON['status'] = True
+    retJSON['message'] = "Successfully created user"
     return HttpResponse(status = 200)
     #include error handles later
 def delete_user(request):
@@ -55,6 +83,11 @@ def createItem(request):
    """ bike = BicycleItem
     for key in request:
             bike.key = request.POST[key]"""
-
+def invalidURL(request):
+    obj= {}
+    obj['status'] = False
+    obj['message'] = "Invalid api request"
+    json_data = json.dumps(obj)
+    return HttpResponse(json_data)
 
 
