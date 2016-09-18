e24df873f03ad75dd4d94795eca130543797baf7
jonathanli28 <jyl3qh@virginia.edu>
2016-09-18 16:44:58 -0400

    changed root urls slightly

--- tangerine/tangerine/urls.py
+++ tangerine/tangerine/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^myapplication/', include('myapplication.urls')),
+    url(r'^$', include('myapplication.urls')),
 ]
