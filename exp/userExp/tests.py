c295c9729696e04425f5390ea3c9911e30632b23
bs5ht <bs5ht@virginia.edu>
2016-10-02 02:19:26 -0400

    testing working, make sure to test in docker container

--- exp/userExp/tests.py
+++ exp/userExp/tests.py
@@ -2,15 +2,14 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 import unittest
 
-
 class GetItemTestCase(TestCase):
   def setUp(self):     #setUp method is called before each test in this class
      pass              #nothing to set uunpit
 
   def test_success_response(self):
-     #response = self.client.get(reverse('itemPage', kwargs={'pk':'1'}))   #assumes user with id 1 is stored in db
-     #self.assertContains(response, 'pk')  #checks that response contains parameter order list & implicitly checks that                                                 #statuscode is 200
-     #self.assertEqual(response.status_code, 200)
+     response = self.client.get(reverse('itemPage', kwargs={'pk':'1'}))   #assumes user with id 1 is stored in db
+     self.assertContains(response, 'pk')  #checks that response contains parameter order list & implicitly checks that                                                 #statuscode is 200
+     self.assertEqual(response.status_code, 200)
     
   def tearDown(self):  #tearDown method is called after each test
      pass  
