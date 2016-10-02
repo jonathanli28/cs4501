c8540ab95aced67800802d58996258f591e35d32
bs5ht <bs5ht@virginia.edu>
2016-10-01 21:39:45 -0400

    made TA changes

--- tangerine/myapplication/urls.py
+++ tangerine/myapplication/urls.py
@@ -3,20 +3,20 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^api/user/get/[0-9]+/?$', views.retrieve_or_modify_user_info),
-    url(r'^api/user/modify/[0-9]+/?$', views.retrieve_or_modify_user_info),
-    url(r'^api/user/delete/[0-9]+/?$', views.delete_user),
-    url(r'^api/user/create/?$', views.create_user),
+    url(r'^api/v1/user/get/[0-9]+/?$', views.retrieve_or_modify_user_info),
+    url(r'^api/v1/user/modify/[0-9]+/?$', views.retrieve_or_modify_user_info),
+    url(r'^api/v1/user/delete/[0-9]+/?$', views.delete_user),
+    url(r'^api/v1/user/create/?$', views.create_user),
 
-    url(r'^api/item/get/[0-9]+/?$', views.retrieve_or_modify_item_info),
-    url(r'^api/item/modify/[0-9]+/?$', views.retrieve_or_modify_item_info),
-    url(r'^api/item/delete/[0-9]+/?$', views.delete_item),
-    url(r'^api/item/create/?$', views.createItem),
+    url(r'^api/v1/item/get/[0-9]+/?$', views.retrieve_or_modify_item_info),
+    url(r'^api/v1/item/modify/[0-9]+/?$', views.retrieve_or_modify_item_info),
+    url(r'^api/v1/item/delete/[0-9]+/?$', views.delete_item),
+    url(r'^api/v1/item/create/?$', views.createItem),
 
-    url(r'^api/itemreview/get/[0-9]+/?$', views.retrieve_or_modify_review),
-    url(r'^api/itemreview/modify/[0-9]+/?$', views.retrieve_or_modify_review),
-    url(r'^api/itemreview/delete/[0-9]+/?$', views.delete_item_review),
-    url(r'^api/itemreview/create/?$', views.createItemReview),
+    url(r'^api/v1/itemreview/get/[0-9]+/?$', views.retrieve_or_modify_review),
+    url(r'^api/v1/itemreview/modify/[0-9]+/?$', views.retrieve_or_modify_review),
+    url(r'^api/v1/itemreview/delete/[0-9]+/?$', views.delete_item_review),
+    url(r'^api/v1/itemreview/create/?$', views.createItemReview),
     #match all other urls
     url ('', views.invalidURL),
     #url(r'^api/items/(?P[0-9]+)/$', views.retrieve_or_modify_item_info),
--- tangerine/myapplication/views.py
+++ tangerine/myapplication/views.py
@@ -1,6 +1,7 @@
 from django.core import serializers
 from .models import User, BicycleItem, ItemReview
 from django.http import HttpResponse
+from django.http import JsonResponse
 import json
 # Create your views here.
 
@@ -20,14 +21,14 @@ def retrieve_or_modify_user_info(request):
             userJSON = {}
             userJSON['status'] = False
             userJSON['message'] = "There is no user specified"
-            userJSON = json.dumps(userJSON)
+            userJSON = userJSON
         else:
             userJSON = serializers.serialize('json', [user,])
             temp = json.loads(userJSON)
             temp[0]['status'] = True
             temp[0]['message'] = "Correctly obtained user"
-            userJSON = json.dumps(temp[0])           
-        return HttpResponse(userJSON)
+            userJSON = temp[0]           
+        return JsonResponse(userJSON)
     elif request.method == 'POST':
         url = request.path
         usrStr = url.split("/")[5]
@@ -41,13 +42,13 @@ def retrieve_or_modify_user_info(request):
             userJSON = serializers.serialize('json', [user,])
             retJSON['status'] = True
             retJSON['message'] = "Successfully modified user"
-            retJSON = json.dumps(retJSON)
+            retJSON = retJSON
         except User.DoesNotExist:
             user = None
             retJSON['status'] = False
             retJSON['message'] = "There is no user specified"
-            retJSON = json.dumps(userJSON)
-        return HttpResponse(retJSON)
+            retJSON = userJSON
+        return JsonResponse(retJSON)
 def create_user(request):
     user = User.objects.create()
     stringster = ""
@@ -57,8 +58,7 @@ def create_user(request):
     retJSON = {}
     retJSON['status'] = True
     retJSON['message'] = "Successfully created user"
-    retJSON = json.dumps(retJSON)
-    return HttpResponse(retJSON)
+    return JsonResponse(retJSON)
     #include error handles later
 def delete_user(request):
     url = request.path
@@ -68,7 +68,7 @@ def delete_user(request):
     retJSON = {}
     retJSON['status'] = True
     retJSON['message'] = "Successfully deleted user"
-    return HttpResponse(retJSON)
+    return JsonResponse(retJSON)
 #url(r'^api/items/(?P<uuid>[^/]+)/$'
 def retrieve_or_modify_item_info(request):
     if request.method == 'GET':
@@ -82,14 +82,13 @@ def retrieve_or_modify_item_info(request):
             bikeJSON = {}
             bikeJSON['status'] = False
             bikeJSON['message'] = "There is no bike specified"
-            bikeJSON = json.dumps(bikeJSON)
         else:
             bikeJSON = serializers.serialize('json', [bike,])
             temp = json.loads(bikeJSON)
             temp[0]['status'] = True
             temp[0]['message'] = "Correctly obtained item"
-            bikeJSON = json.dumps(temp[0])
-        return HttpResponse(bikeJSON)
+            bikeJSON = temp[0]
+        return JsonResponse(bikeJSON)
     elif request.method == 'POST':
         url = request.path
         bikeStr = url.split("/")[5]
@@ -102,15 +101,13 @@ def retrieve_or_modify_item_info(request):
         if bike == None:
             retJSON['status'] = False
             retJSON['message'] = "There is no bike specified"
-            retJSON = json.dumps(bikeJSON)
         else:
             for key in request.POST:
                 setattr(bike, key, request.POST.get(key, False))
             bike.save()
             retJSON['status'] = True
             retJSON['message'] = "Successfully modified bike"
-            retJSON = json.dumps(retJSON)
-        return HttpResponse(retJSON)
+        return JsonResponse(retJSON)
 
 def createItem(request):
     bike = BicycleItem.objects.create()
@@ -121,8 +118,7 @@ def createItem(request):
     retJSON = {}
     retJSON['status'] = True
     retJSON['message'] = "Successfully created bike"
-    retJSON = json.dumps(retJSON)
-    return HttpResponse(retJSON)
+    return JsonResponse(retJSON)
 
 def delete_item(request):
     url = request.path
@@ -132,8 +128,7 @@ def delete_item(request):
     retJSON = {}
     retJSON['status'] = True
     retJSON['message'] = "Successfully deleted bike"
-    retJSON = json.dumps(retJSON)
-    return HttpResponse(retJSON)
+    return JsonResponse(retJSON)
 
 def retrieve_or_modify_review(request):
     if request.method == 'GET':
@@ -147,14 +142,12 @@ def retrieve_or_modify_review(request):
             reviewJSON = {}
             reviewJSON['status'] = False
             reviewJSON['message'] = "There is no review specified"
-            reviewJSON = json.dumps(reviewJSON)
         else:
             reviewJSON = serializers.serialize('json', [review,])
             temp = json.loads(reviewJSON)
             temp[0]['status'] = True
             temp[0]['message'] = "Correctly obtained review"
-            reviewJSON = json.dumps(temp[0])
-        return HttpResponse(reviewJSON)
+        return JsonResponse(reviewJSON)
     elif request.method == 'POST':
         url = request.path
         reviewStr = url.split("/")[5]
@@ -167,15 +160,13 @@ def retrieve_or_modify_review(request):
         if review == None:
             retJSON['status'] = False
             retJSON['message'] = "There is no review specified"
-            retJSON = json.dumps(reviewJSON)
         else:
             for key in request.POST:
                 setattr(review, key, request.POST.get(key, False))
             review.save()
             retJSON['status'] = True
             retJSON['message'] = "Successfully modified review"
-            retJSON = json.dumps(retJSON)
-        return HttpResponse(retJSON)
+        return JsonResponse(retJSON)
 def createItemReview(request):
     review = ItemReview.objects.create()
 
@@ -185,8 +176,7 @@ def createItemReview(request):
     retJSON = {}
     retJSON['status'] = True
     retJSON['message'] = "Successfully created review"
-    retJSON = json.dumps(retJSON)
-    return HttpResponse(retJSON)
+    return JsonResponse(retJSON)
 
 def delete_item_review(request):
     url = request.path
@@ -196,15 +186,13 @@ def delete_item_review(request):
     retJSON = {}
     retJSON['status'] = True
     retJSON['message'] = "Successfully deleted bike"
-    retJSON = json.dumps(retJSON)
-    return HttpResponse(retJSON)
+    return JsonResponse(retJSON)
 
 
 def invalidURL(request):
     obj= {}
     obj['status'] = False
     obj['message'] = "Invalid api request"
-    json_data = json.dumps(obj)
-    return HttpResponse(json_data)
+    return JsonResponse(json_data)
 
 
