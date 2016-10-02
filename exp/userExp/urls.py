ce8dbebb08b25f4f30d6b2700dadbe2576456679
bs5ht <bs5ht@virginia.edu>
2016-10-01 23:56:30 -0400

    homepage and itempage method added

--- exp/userExp/urls.py
+++ exp/userExp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
         url(r'^api/v1/homepage', views.homePageData, name = 'homePage'),
-        url(r'^api/v1/itempage', views.individualItemData, name = 'itemPage'),
+        url(r'^api/v1/itempage/(?P<pk>[0-9]+)/?', views.individualItemData, name = 'itemPage'),
+         url ('', views.invalidURL),
 ]
--- exp/userExp/views.py
+++ exp/userExp/views.py
@@ -4,32 +4,35 @@ from django.http import JsonResponse
 
 # Create your views here.
 
+
 def homePageData(request):
     if request.method == 'GET':
         list = []
-        urlForLastItem = "http://localhost:8001/api/v1/item/get/"
-        urlForLatest = "http://localhost:8001/api/v1/item/getlatest"
+        urlForLastItem = "http://models-api:8000/api/v1/item/get/"
+        urlForLatest = "http://models-api:8000/api/v1/item/getlatest"
         
         req = urllib.request.Request(urlForLatest)
         ret = urllib.request.urlopen(req).read().decode('utf-8')
-        latest = json.loads(ret.read)
-
+        latest = json.loads(ret)
+        
         for x in range(0, 3):
-            rearNumber = latest["fields"]["pk"]
+            rearNumber = int(latest["pk"])
             requester = urllib.request.Request(urlForLastItem + str(rearNumber - x))
             response = urllib.request.urlopen(requester).read().decode('utf-8')
-            bikeItem = json.load(response.read())
+            bikeItem = json.loads(response)
             list.append(bikeItem)
 
         final = { 'bike1' : list[0], 'bike2' : list[1], 'bike3' : list[2] }
+        
         return JsonResponse(final)
 
-def individualItemData(request):
-    if request.method == 'POST':
-        itemPK = request.POST.get('item', '')
-        urlForParticularItem = "http://localhost:8001/api/v1/item/get/"
-        response = urllib.request.urlopen(urlForParticularItem + str(itemPK[0]))
-        bikeItem = json.loads(response.read)
+
+def individualItemData(request, pk):
+    if request.method == 'GET':
+        urlForParticularItem = "http://models-api:8000/api/v1/item/get/"
+        requester = urllib.request.Request(urlForParticularItem + pk)
+        response = urllib.request.urlopen(requester).read().decode('utf-8')
+        bikeItem = json.loads(response)
         return JsonResponse(bikeItem)
 
 def invalidURL(request):
