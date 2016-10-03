5ae227e07d9f9ad7c545378ee23943bc9d81e82a
Jonathan Li <jyl3qh@virginia.edu>
2016-10-02 21:45:46 -0400

    front end views

--- web/frontEnd/urls.py
+++ web/frontEnd/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    #url(r'^api/items/(?P[0-9]+)/$', views.retrieve_or_modify_item_info),
+    url(r'^home', views.homepageSplash, name='homePageSplash'),
+    url(r'^item', views.itempageSplash, name='itemPageSplash'),
+    url(r'^about', views.aboutSplash, name='aboutSplash'),
+    url ('', views.invalidURL),
     
 ]
--- web/frontEnd/views.py
+++ web/frontEnd/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import urllib.request, json
+from django.http import HttpResponseNotFound
 
 # Create your views here.
+
+def homepageSplash(request):
+    if request.method == 'GET':
+        urlForExpLayer = "http://exp-api:8001/api/v1/homepage"
+        req = urllib.request.Request(urlForExpLayer)
+        ret = urllib.request.urlopen(req).read().decode('utf-8')
+        latest = json.loads(ret)
+        return render(request, "templates/index.html", latest)
+
+def itempageSplash(request, pk):
+    if request.method == 'GET':
+        urlForExpLayer = "http://exp-api:8001api/v1/itempage/"
+        req = urllib.request.Request(urlForExpLayer + pk)
+        ret = urllib.request.urlopen(req).read().decode('utf-8')
+        latest = json.loads(ret)
+        return render(request, "templates/blist.html", latest)
+
+def aboutSplash(request):
+    return render(request, "templates/about.html")
+
+def invalidURL(request):
+    return HttpResponseNotFound
