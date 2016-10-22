c8857b950f8148e5ba99f38c70aff02ce2d0166b
bs5ht <bs5ht@virginia.edu>
2016-10-22 19:12:56 -0400

    models layer almost done

--- tangerine/myapplication/models.py
+++ tangerine/myapplication/models.py
@@ -6,11 +6,12 @@ class User(models.Model):
     username = models.CharField(max_length=360)
     first_name = models.CharField(max_length=360)
     last_name = models.CharField(max_length=360)
+    passwd = models.CharField(max_length=360)
     date_created = models.DateTimeField(auto_now_add=True)
 
 class Authenticator(models.Model):
-    user_id = models.CharField(max_length=360)
-    authenticator = models.CharField(max_length=360, primary_key=True)
+    username = models.CharField(max_length=360)
+    authenticator = models.CharField(max_length=255, primary_key=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
 class BicycleItem(models.Model):
--- tangerine/myapplication/views.py
+++ tangerine/myapplication/views.py
@@ -1,7 +1,13 @@
 from django.core import serializers
-from .models import User, BicycleItem, ItemReview
+from .models import User, Authenticator, BicycleItem, ItemReview
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.contrib.auth import hashers
+import os
+import hmac
+# import django settings file
+import settings
+
 import json
 # Create your views here.
 
@@ -47,11 +53,24 @@ def retrieve_or_modify_user_info(request):
             retJSON['message'] = "There is no user specified"
             retJSON = userJSON
         return JsonResponse(retJSON)
+
 def create_user(request):
-    user = User.objects.create()
-    stringster = ""
-    for key in request.POST:
-        setattr(user, key, request.POST.get(key, False))
+
+    try:
+        user = User.objects.get(username = request.POST['username'])
+        if(user != None)
+            retJSON = {}
+            retJSON['status'] = False
+            retJSON['message'] = "username already defined in database"
+            return JsonResponse(retJSON)
+
+    user = models.User(username=request.POST['username'],                         
+        first_name=request.POST['first_name'],                             
+        last_name=request.POST['last_name'],                             
+        passwd=hashers.make_password(request.POST['passwd']),                                         \
+        date_created=datetime.datetime.now()                        
+        )
+
     user.save()
     retJSON = {}
     retJSON['status'] = True
@@ -67,6 +86,79 @@ def delete_user(request):
     retJSON['status'] = True
     retJSON['message'] = "Successfully deleted user"
     return JsonResponse(retJSON)
+
+def create_auth(request):
+    try:
+        user = User.objects.get(username = request.POST['username'])
+    except User.DoesNotExist:
+        user = None
+    if user == None:
+        userJSON = {}
+        userJSON['status'] = False
+        userJSON['message'] = "There is no user specified"
+        return JsonResponse(userJSON)
+    retJSON = {}
+    temp_passwd = hashers.make_password(request.POST['passwd'])
+    if(user.passwd == temp_passwd):
+
+        #creating the authenticator
+        auth= hmac.new(key = settings.SECRET_KEY.encode('utf-8'), 
+            msg = os.urandom(32), digestmod = 'sha256').hexdigest()
+
+        a = models.Authenticator(user_id=request.POST['user_id'],                                                     
+            authenticator= auth,                                        
+            date_created=datetime.datetime.now()                        
+            )
+        a.save()
+        retJSON['status'] = True
+        retJSON['message'] = "Successfully created authenticator"
+    else:
+        retJSON['status'] = False
+        retJSON['message'] = "Failed to create authenticator"
+    return JsonResponse(retJSON)
+
+def check_auth(request):
+    retJSON = {}
+    try:
+        a = Authenticator.objects.get(authenticator = request.POST['auth'])
+    except Authenticator.DoesNotExist:
+        a = None
+    if(a == None):
+        retJSON['status'] = False
+        retJSON['message'] = "There is no Authenticator in db"
+        return JsonResponse(retJSON)
+    
+    if(a.username == request.POST['username']):
+        timeDelta = (datetime.datetime.now() - a.date_created).days * 24 * 60
+        if(timeDelta > 60 * 2): #set authenticator time to be two hours 
+            retJSON['status'] = False
+            retJSON['message'] = "Authenticator expired"
+        else:
+            retJSON['status'] = True
+            retJSON['message'] = "Authenticator valid"
+    else:
+        retJSON['status'] = True
+        retJSON['message'] = "Authenticator invalid(mismatch)"
+    return JsonResponse(retJSON)
+
+def delete_auth(request):
+    retJSON = {}
+    try:
+        a = Authenticator.objects.get(authenticator = request.POST['auth'])
+    except Authenticator.DoesNotExist:
+        a = None
+    if(a == None):
+        retJSON['status'] = False
+        retJSON['message'] = "There is no Authenticator in db"
+        return JsonResponse(retJSON)
+    a.delete()
+    retJSON = {}
+    retJSON['status'] = True
+    retJSON['message'] = "Successfully deleted auth"
+    return JsonResponse(retJSON)
+
+
+
 
 #url(r'^api/items/(?P<uuid>[^/]+)/$'
 def retrieve_or_modify_item_info(request):
