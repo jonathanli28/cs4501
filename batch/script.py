a4c84fa12f946cd397b84542d633bd7990d4d832
bs5ht <bs5ht@virginia.edu>
2016-10-30 16:13:26 -0400

    added script to add to elastic search

---
+++ batch/script.py
@@ -0,0 +1,20 @@
+import sched, time
+from kafka import KafkaConsumer
+import sched, time
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch([{'host': 'es', 'port': 9200}])
+
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc): 
+	print("hello")
+	consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+	for message in consumer:
+		new_listing = json.loads((message.value).decode('utf-8'))
+		ret = es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
+	es.indices.refresh(index="listing_index")
+
+	s.enter(60, 1, do_something, (sc,))
+
+s.enter(60, 1, do_something, (s,))
+s.run()
